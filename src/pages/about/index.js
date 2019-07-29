81722956aaa3d195abf66cd1336f6b90f71d1cbb
michael levy <mike.levy75@gmail.com>
2019-07-28 22:27:09 -0400

    comment out

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+// import { graphql } from 'gatsby';
 
 import { PageTemplate, Paragraph } from 'components';
 
 const About = ({ data }) => {
-  const { allMarkdownRemark } = data; // data.markdownRemark holds our post data
-  const { frontmatter } = allMarkdownRemark.nodes[0];
-  console.log(frontmatter);
+  // const { allMarkdownRemark } = data; // data.markdownRemark holds our post data
+  // const { frontmatter } = allMarkdownRemark.nodes[0];
+  // console.log(frontmatter);
 
   return (
     <PageTemplate>
@@ -15,19 +15,19 @@ const About = ({ data }) => {
   )
 };
 
-export const pageQuery = graphql`
-  query {
-    allMarkdownRemark(filter: {frontmatter: {title: {eq: "About Page"}}}) {
-      nodes {
-        frontmatter {
-          title
-          pageHeader
-          entryHeader
-        }
-        rawMarkdownBody
-      }
-    }
-  }
-`;
+// export const pageQuery = graphql`
+//   query {
+//     allMarkdownRemark(filter: {frontmatter: {title: {eq: "About Page"}}}) {
+//       nodes {
+//         frontmatter {
+//           title
+//           pageHeader
+//           entryHeader
+//         }
+//         rawMarkdownBody
+//       }
+//     }
+//   }
+// `;
 
 export default About;
