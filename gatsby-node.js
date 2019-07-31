f318db2423bd910bcaa2352882999c5116105b8b
michael levy <mike.levy75@gmail.com>
2019-07-30 22:21:08 -0400

    changes

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,57 +10,56 @@ exports.onCreateWebpackConfig = ({ stage, actions }) => {
   })
 }
 
-/* exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions;
+// exports.createPages = ({ actions, graphql }) => {
+//   const { createPage } = actions;
 
-  return graphql(`
-    {
-      allMarkdownRemark(limit: 1000) {
-        edges {
-          node {
-            id
-            fields {
-              slug
-            }
-            frontmatter {
-              templateKey
-            }
-          }
-        }
-      }
-    }
-  `).then(result => {
-    if (result.errors) {
-      result.errors.forEach(e => console.error(e.toString()))
-      return Promise.reject(result.errors)
-    }
+//   return graphql(`
+//     {
+//       allMarkdownRemark(limit: 1000) {
+//         edges {
+//           node {
+//             id
+//             fields {
+//               slug
+//             }
+//             frontmatter {
+//               templateKey
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `).then(result => {
+//     if (result.errors) {
+//       result.errors.forEach(e => console.error(e.toString()))
+//       return Promise.reject(result.errors)
+//     }
 
-    const posts = result.data.allMarkdownRemark.edges
+//     const posts = result.data.allMarkdownRemark.edges
 
-    posts.forEach(edge => {
-      const id = edge.node.id;
-      createPage({
-        path: edge.node.fields.slug,
-        component: path.resolve(
-          `src/pages/${String(edge.node.frontmatter.templateKey)}`
-        ),
-        // additional data can be passed via context
-        context: {
-          id,
-        },
-      })
-    })
-  });
-}; */
+//     posts.forEach(edge => {
+//       const id = edge.node.id;
+//       createPage({
+//         path: edge.node.fields.slug,
+//         component: path.resolve(
+//           `src/pages/${String(edge.node.frontmatter.templateKey)}`
+//         ),
+//         // additional data can be passed via context
+//         context: {
+//           id,
+//         },
+//       })
+//     })
+//   });
+// };
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
   // fmImagesToRelative(node) // convert image paths for gatsby images
 
   if (node.internal.type === `MarkdownRemark`) {
-    const filePath = createFilePath({ node, getNode }).toLowerCase().replace('page', '');
-    const value = filePath === '/home/' ? '/' : filePath.replace(/\/$/, '')
-                                                        .replace('portfolio-', '');
+    const value = createFilePath({ node, getNode });
+
     createNodeField({
       name: `slug`,
       node,
--- src/pages/portfolio/entries/portfolio-test-entry.md
+++
@@ -1,6 +0,0 @@
----
-templateKey: src/pages/PortfolioPage/entry.js
-title: 'test entry '
-created: 2019-07-31T01:54:38.827Z
----
-test entry copy
