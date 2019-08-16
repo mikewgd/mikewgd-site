cd261c6420afacb7ad043300000c02c836996448
michael levy <mike.levy75@gmail.com>
2019-08-16 16:09:21 -0400

    Auto stash before merge of "dev" and "origin/dev"

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import CMS from 'netlify-cms-app';
 import cloudinary from 'netlify-cms-media-library-cloudinary';
 
@@ -8,6 +9,34 @@ import cloudinary from 'netlify-cms-media-library-cloudinary';
 
 // CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
+
+CMS.registerEditorComponent({
+  // Internal id of the component
+  id: "youtube",
+  // Visible label
+  label: "Youtube",
+  // Fields the user need to fill out when adding an instance of the component
+  fields: [{name: 'id', label: 'Youtube Video ID', widget: 'string'}],
+  // Pattern to identify a block as being an instance of this component
+  pattern: /^youtube (\S+)$/,
+  // Function to extract data elements from the regexp match
+  fromBlock: function(match) {
+    return {
+      id: match[1]
+    };
+  },
+  // Function to create a text block from an instance of this component
+  toBlock: function(obj) {
+    return 'youtube ' + obj.id;
+  },
+  // Preview output for this component. Can either be a string or a React component
+  // (component gives better render performance)
+  toPreview: function(obj) {
+    return (
+      '<img src="http://img.youtube.com/vi/' + obj.id + '/maxresdefault.jpg" alt="Youtube Video"/>'
+    );
+  }
+});
 
 // CMS.registerPreviewTemplate('index', IndexPagePreview)
 // CMS.registerPreviewTemplate('about', AboutPagePreview)
